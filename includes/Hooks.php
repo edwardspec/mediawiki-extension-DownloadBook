66aadfd42f69c1b604c2983cc93afc5a4336753e
Edward Chernenko <edwardspec@gmail.com>
2020-03-31 01:33:08 +0300

    Add username "DownloadBookStash" to the list of reserved usernames

---
+++ includes/Hooks.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+	Extension:DownloadBook - MediaWiki extension.
+	Copyright (C) 2020 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+ * @file
+ * Hooks used by Extension:DownloadBook.
+ */
+
+namespace MediaWiki\DownloadBook;
+
+class Hooks {
+	/**
+	 * Mark username "DownloadBookStash" as reserved.
+	 * @param string[] $reservedUsernames
+	 * @return bool
+	 */
+	public static function onUserGetReservedNames( array $reservedUsernames ) {
+		$reservedUsernames[] = 'DownloadBookStash';
+		return true;
+	}
+}
