f3d9512db1ca2167b8ba01da1d8a0d168595a5b9
Edward Chernenko <edwardspec@gmail.com>
2024-12-16 22:29:13 +0300

    (tests) Run phpunit via GitHub Actions

---
+++ tests/phpunit/SpecialDownloadBookTest.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+	Extension:DownloadBook - MediaWiki extension.
+	Copyright (C) 2024 Edward Chernenko.
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+*/
+
+/**
+ * @file
+ * Integration test for [[Special:DownloadBook]] (API expected by Extension:Collections).
+ */
+
+namespace MediaWiki\DownloadBook;
+
+use FauxRequest;
+use MWException;
+use SpecialPageTestBase;
+
+/**
+ * @covers MediaWiki\DownloadBook\SpecialDownloadBook
+ * @group Database
+ */
+class SpecialDownloadBookTest extends SpecialPageTestBase {
+	protected function newSpecialPage() {
+		return new SpecialDownloadBook();
+	}
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->tablesUsed[] = 'bookrenderingtask';
+	}
+
+	/**
+	 * Checks the result when called without ?command=.
+	 */
+	public function testErrorNoCommand() {
+		$this->expectExceptionObject( new MWException( 'Unknown command.' ) );
+		$this->runSpecial( [] );
+	}
+
+	/**
+	 * Checks the result when called with unsupported ?command=.
+	 */
+	public function testErrorUnknownCommand() {
+		$this->expectExceptionObject( new MWException( 'Unknown command.' ) );
+		$this->runSpecial( [ 'command' => 'makesalad' ] );
+	}
+
+	// TODO: integration test for [ 'command' => 'render' ]
+	// TODO: integration test for [ 'command' => 'render_status' ]
+	// TODO: integration test for [ 'command' => 'stream' ]
+
+	/**
+	 * Render Special:DownloadBook.
+	 * @param array $query Query string parameters.
+	 * @return HTML of the result.
+	 */
+	public function runSpecial( array $query ) {
+		[ $html, ] = $this->executeSpecialPage( '', new FauxRequest( $query, false ) );
+		return $html;
+	}
+}
